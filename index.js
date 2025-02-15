91b3ce934a3c3337cfea634ac86620563e2054c5
unknown <moazzemh222@gmail.com>
2025-02-15 14:54:53 +0600

    Access and Modifier using js file

---
+++ index.js
@@ -0,0 +1,9 @@
+const data = require("./friends_data.json");
+//console.log(data);
+
+//console.log(data.friends[0]);
+//console.log(data.friends[0].name);
+
+
+data.friends[0].name = "Zem";
+console.log(data.friends[0].name);
