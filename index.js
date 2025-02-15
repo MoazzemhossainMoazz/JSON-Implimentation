1e91a4b04e08ba232c82e0815c44a0de8842ddb9
unknown <moazzemh222@gmail.com>
2025-02-15 14:58:40 +0600

    Delete file

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const data = require("./friends_data.json");
+//Data Access
 //console.log(data);
 
 //console.log(data.friends[0]);
 //console.log(data.friends[0].name);
 
+//data.friends[0].name = "Zem";
+//console.log(data.friends[0].name);
 
-data.friends[0].name = "Zem";
-console.log(data.friends[0].name);
+
+
+
+//Data Modify
+delete data.friends[0].name;
+console.log(data);
